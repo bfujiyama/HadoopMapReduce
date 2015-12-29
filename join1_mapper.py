e9bac9c3a5fe7e4406fd2a61e246c048c82b4196
bfujiyama <bfujiyama@gmail.com>
2015-12-29 02:49:05 -0800

    Update join1_mapper.py

--- join1_mapper.py
+++ join1_mapper.py
@@ -13,16 +13,5 @@ for line in sys.stdin:
     elif (value_in == "ABC"):
     	print( '%s\t%s' % (key_in, value_in) )  #print a string, tab, and string
 
-
-
-#for line in sys.stdin:
-#    line       = line.strip()   #strip out carriage return
-#    key_value  = line.split(",")   #split line, into key and value, returns a list
-#    key_in     = key_value[0]  #key is first item in list
-#    value_in   = key_value[1]   #value is 2nd item 
-
-#	if (key_in == "Almost_Show"):
-#		print('%s\t%s' % (key_in, value_in))	#print a string, tab and string
-
 #Note that Hadoop expects a tab to separate key value
 #but this program assumes the input file has a ',' separating key value
